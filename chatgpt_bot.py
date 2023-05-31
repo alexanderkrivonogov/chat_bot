51539ae4e1c1056221d086a26a6dab0a1b9edfb5
alexanderkrivonogov <souspremium@gmail.com>
2023-05-31 13:29:05 +0500

    Add chat history

--- chatgpt_bot.py
+++ chatgpt_bot.py
@@ -9,21 +9,35 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 openai.api_key = TOKEN_OPENAI
 
+dialog = []  # Переменная для сохранения состояния диалога
+
 
 @dp.message_handler()
 async def handle_message(message: types.Message):
+    global dialog  # Объявляем переменную dialog как глобальную, чтобы иметь доступ к ней из функции
+    # print(message.text)
     user_input = message.text
-    # Отправляем запрос на модель GPT-3.5 Turbo
+
+    # Добавляем сообщение пользователя в диалог
+    dialog.append({"role": "user", "content": user_input})
+
+    # Отправляем запрос на модель GPT-3.5 Turbo с полным диалогом
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Вы:"},
-            {"role": "user", "content": user_input},
+            {"role": "system", "content": "You are a helpful assistant"},
+            *dialog  # Добавляем предыдущие сообщения диалога
         ]
     )
     # Получаем ответ от модели
     answer = response.choices[0].message.content
+
+    # Добавляем ответ ассистента в диалог
+    dialog.append({"role": "assistant", "content": answer})
+
     # Отправляем ответ пользователю
+    # print(response)
+    # print(dialog)
     await message.reply(answer)
 
 
