14f1f0f893981628df713130350a01a502f573c1
alexanderkrivonogov <souspremium@gmail.com>
2023-06-09 13:39:35 +0500

    Create disbot.py

---
+++ disbot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()  # Создание объекта intents
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+@bot.command()  # Не передаём аргумент pass_context, так как он был нужен в старых версиях.
+async def hello(ctx):  # Создаём функцию и передаём аргумент ctx.
+    author = ctx.message.author  # Объявляем переменную author и записываем туда информацию об авторе.
+    print(author)
+    await ctx.send(
+        f'Hello, {author.mention}!')  # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
+
+
+bot.run(
+    'MTExNjI4NDIwMzE3MzE1NDg5OQ.GNzhjK.9s1OsTQQkH7VBs4BGuzHrWaALBAdEuuf_NFvI8')  # Обращаемся к словарю settings с ключом token, для получения токена
